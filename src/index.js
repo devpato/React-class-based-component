312f3158b458513fef9f80903fb8c9d4ddc1bc52
pevargasg <wearetamo@gmail.com>
2019-02-18 22:53:37 -0800

    using geolocation api

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    position => console.log(position),
+    error => console.log(error)
+  );
+  return <div>Latitude: </div>;
+};
+
 ReactDOM.render(<App />, document.getElementById("root"));
